e6c7302218e3c6bb0f09b59d24de7d228371e555
xukai <xukai@netjoy.com>
2020-01-06 14:44:12 +0800

    release config.py

---
+++ config.py
@@ -0,0 +1,27 @@
+"""
+    Created by xukai on 2019/12/24
+"""
+
+# 查询天气的城市
+CITY_NAME = '上海'
+
+# 每日一句渠道(1:土味情话,2彩虹屁)
+WORDS_CHANNEL = 2
+
+# 计算在一起的天数,日期不填写则不计算
+START_DATE = ''
+START_DATE_MSG = '爱你的第{}天'
+
+# 爱称
+YOUR_NAME = ''
+MY_NAME = ''
+
+# Email配置
+MAIL_SERVER = 'smtp.qq.com'
+MAIL_PORT = 25
+MAIL_USE_SSL = True
+MAIL_USE_TSL = False
+MAIL_USERNAME = ''
+MAIL_PASSWORD = ''
+MAIL_SUBJECT_PREFIX = '[每日天气]'
+MAIL_RECEIVERS = ''  # 接收人邮箱
