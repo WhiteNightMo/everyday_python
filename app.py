7aa077a9b531010b77fcad441f166f8f7338d55b
xukai <xukai@netjoy.com>
2020-01-07 11:28:43 +0800

    记录运行日志

--- app.py
+++ app.py
@@ -3,11 +3,13 @@
 """
 from config import *
 from utils.email import send_mail
-from utils.helper import get_diff_time
+from utils.helper import get_diff_time, init_logger
 from utils.weather import get_weather_info
 from utils.one_words import get_one_words
 
 if __name__ == '__main__':
+    logger = init_logger()
+
     # 获取一句彩虹屁
     words = get_one_words(WORDS_CHANNEL)
     # 获取天气信息
@@ -21,5 +23,4 @@ if __name__ == '__main__':
     send_msg = '\n'.join(x for x in [words, weather, diff_time, pet_name] if x)
     # 发送邮件
     send_mail(to=YOUR_NAME, receiver=MAIL_RECEIVERS, msg=send_msg)
-    print('\n------------------\n' + send_msg + '\n')
-    pass
+    logger.info('\n------------------\n' + send_msg + '\n')
--- utils/helper.py
+++ utils/helper.py
@@ -1,7 +1,10 @@
 """
     Created by xukai on 2019/12/24
 """
+import logging
+import os
 import re
+import sys
 from datetime import datetime
 
 
@@ -26,3 +29,33 @@ def get_diff_time(start_date, start_msg=''):
     else:
         delta_msg = '宝贝这是我们在一起的第 {} 天。'.format(day_delta)
     return delta_msg
+
+
+def init_logger():
+    path = r"./logs/"
+    if not os.path.exists(path):
+        os.makedirs(path)
+    path += datetime.now().strftime('%Y-%m-%d') + ".log"
+
+    logger = logging.getLogger('log')
+    logger.setLevel(logging.DEBUG)
+
+    # 调用模块时,如果错误引用，比如多次调用，每次会添加Handler，造成重复日志，这边每次都移除掉所有的handler，后面在重新添加，可以解决这类问题
+    while logger.hasHandlers():
+        for i in logger.handlers:
+            logger.removeHandler(i)
+
+    # file log
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    fh = logging.FileHandler(path, encoding='utf-8')
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+    # console log
+    formatter = logging.Formatter('%(message)s')
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(logging.DEBUG)
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    return logger
